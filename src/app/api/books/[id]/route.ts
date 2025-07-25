63bfc3cb1d3815d86bb44af82ac5d484fdb9c073
João Vitor Nunes da Silva <135175090+Joao-Vitor-Nunes@users.noreply.github.com>
2025-07-25 12:45:23 -0400

    v2

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,17 +1,32 @@
+// src/app/api/books/[id]/route.ts
 import { prisma } from '../../../../../lib/prisma'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  const body = await req.json()
-  const book = await prisma.book.update({
-    where: { id: params.id },
-    data: body,
-  })
-  return NextResponse.json(book)
+// Atualizar livro
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+  const data = await request.json()
+
+  try {
+    const book = await prisma.book.update({
+      where: { id: params.id },
+      data,
+    })
+
+    return NextResponse.json(book)
+  } catch (error) {
+    return NextResponse.json({ error: 'Erro ao atualizar livro.' }, { status: 500 })
+  }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  await prisma.book.delete({ where: { id: params.id } })
-  return NextResponse.json({ message: 'Livro deletado com sucesso!' })
-}
+// Deletar livro
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    await prisma.book.delete({
+      where: { id: params.id },
+    })
 
+    return NextResponse.json({ message: 'Livro deletado com sucesso!' })
+  } catch (error) {
+    return NextResponse.json({ error: 'Erro ao deletar livro.' }, { status: 500 })
+  }
+}
